6684ebb1419e18bbbd132eee5c675dbe895d59f7
pranav6266 <pranavchandrashekar5@gmail.com>
2025-07-08 21:46:06 +0530

    added springboot dependency

---
+++ src/main/java/com/pranav/temple_software/config/AppConfig.java
@@ -0,0 +1,9 @@
+package com.pranav.temple_software.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "com.pranav.temple_software")
+public class AppConfig {
+}
